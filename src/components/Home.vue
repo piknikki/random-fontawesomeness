06ad3f1664f546d187305b6a4315235712414df1
Nikki Petersen <piknik@me.com>
2020-08-12 11:04:37 -0600

    linked up icon refresh works now

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <h1>Random Fontawesome Icon Generator</h1>
-    <button class="button is-normal">Make it so</button>
+    <h1 class="title">Random Fontawesome Icon Generator</h1>
+    <button @click="generate()" class="button is-normal">Make it so</button>
     <br>
-      <span class="icon is-large">
-        <i class="fal fa-magic is-large"></i>
+    <div class="container box">
+      <span id="fa-icon" v-if="selectedIcon">
+        <i :class="selectedIcon"></i>
       </span>
+    </div>
+
   </div>
 </template>
 
@@ -14,12 +17,35 @@ export default {
   name: 'home',
   data () {
     return {
-      icon: ''
+      iconArray: [
+        'fal fa-abacus',
+        'fal fa-magic',
+        'fal fa-alicorn'
+      ],
+      selectedIcon: null
     }
+  },
+  methods: {
+    randomIcon (items) {
+      return items[Math.floor(Math.random() * items.length)]
+    },
+    generate () {
+      this.selectedIcon = this.randomIcon(this.iconArray)
+    }
+  },
+  created () {
+    this.selectedIcon = this.randomIcon(this.iconArray)
   }
 }
 </script>
 
 <style scoped>
+.box {
+  padding: 100px;
+}
+
+fa-icon {
+  width: 50px;
+}
 
 </style>
