27edd58073533e15c087f3844e62abb711cc8039
Nikki Petersen <piknik@me.com>
2020-08-12 15:57:33 -0600

    forget the button for now just refresh

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,95 @@
 <template>
   <div id="app">
     <img id="logo" alt="Vue logo" src="./assets/logo.png">
-  <Home />
+    <h1 class="title">Random Fontawesome Icon Generator</h1>
+    <p>(click refresh on your browser to see a new one)</p>
+<!--    <button v-on:click="generate()" class="button is-normal">Make it so</button>-->
+    <br>
+    <div class="container box">
+      <span id="fa-icon" class="icon is-large is-vcentered">
+<!--        <pre>{{ htmlCode() }}</pre>-->
+           <i :class="selectedIcon + ' fa-7x'"></i>
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
-
-import Home from '@/components/Home'
 export default {
-  name: 'App',
-  components: { Home }
+  name: 'app',
+  data () {
+    return {
+      iconArray: [
+        'fal fa-abacus',
+        'fal fa-magic',
+        'fal fa-alicorn',
+        'fal fa-angry',
+        'fal fa-amp-guitar',
+        'fal fa-badger-honey',
+        'fal fa-bacon',
+        'fal fa-bone-break',
+        'fal fa-blender-phone',
+        'fal fa-bomb',
+        'fal fa-bong',
+        'fal fa-bug',
+        'fal fa-alien',
+        'fal fa-cat-space',
+        'fal fa-comet',
+        'fal fa-flux-capacitor',
+        'fal fa-meteor',
+        'fal fa-portal-enter',
+        'fal fa-space-station-moon-alt',
+        'fal fa-starship-freighter',
+        'fal fa-dagger',
+        'fal fa-dragon',
+        'fal fa-flask-potion',
+        'fal fa-skull-crossbones',
+        'fal fa-wand-magic',
+        'fal fa-cow',
+        'fal fa-deer-rudolph',
+        'fal fa-dog',
+        'fal fa-frog',
+        'fal fa-narwhal',
+        'fal fa-otter',
+        'fal fa-sheep',
+        'fal fa-skull-crossbones',
+        'fal fa-sparkles',
+        'fal fa-cookie-bite',
+        'fal fa-coffee-pot',
+        'fal fa-code-branch',
+        'fal fa-cloud-meatball',
+        'fal fa-code-branch',
+        'fal fa-code-commit',
+        'fal fa-female',
+        'fal fa-fort-awesome',
+        'fal fa-eye',
+        'fal fa-flushed',
+        'fal fa-flask-poison',
+        'fal fa-grin-squint-tears',
+        'fal fa-grin-tongue',
+        'fal fa-grimace'
+      ],
+      selectedIcon: this.selectedIcon
+    }
+  },
+  methods: {
+    // htmlCode () {
+    //   let html = '<i class="fa-7x '
+    //   html += this.selectedIcon
+    //   html += '"></i>'
+    //   return html
+    // },
+    randomItem (items) {
+      return items[Math.floor(Math.random() * items.length)]
+    },
+    generate () {
+      console.log(this.selectedIcon)
+      this.selectedIcon = this.randomItem(this.iconArray)
+    }
+  },
+  created () {
+    this.selectedIcon = this.randomItem(this.iconArray)
+  }
 }
 </script>
 
@@ -26,5 +105,14 @@ export default {
 
 #logo {
   width: 100px;
+}
+
+button {
+  margin: 20px;
+}
+.box {
+  width: 300px;
+  height: 300px;
+  padding: 100px;
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,53 +1,13 @@
 <template>
-  <div>
-    <h1 class="title">Random Fontawesome Icon Generator</h1>
-    <button @click="generate()" class="button is-normal">Make it so</button>
-    <br>
-    <div class="container box">
-      <span id="fa-icon" class="icon is-large is-vcentered">
-        <i :class="selectedIcon + ' fa-7x'"></i>
-      </span>
-    </div>
 
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  data () {
-    return {
-      iconArray: [
-        'fal fa-abacus',
-        'fal fa-magic',
-        'fal fa-alicorn',
-        'fal fa-angry'
-      ],
-      selectedIcon: null
-    }
-  },
-  methods: {
-    randomIcon (items) {
-      return items[Math.floor(Math.random() * items.length)]
-    },
-    generate () {
-      this.selectedIcon = this.randomIcon(this.iconArray)
-    }
-  },
-  created () {
-    this.selectedIcon = this.randomIcon(this.iconArray)
-  }
+  name: 'home'
 }
 </script>
 
 <style scoped>
-button {
-  margin: 20px;
-}
-.box {
-  width: 300px;
-  height: 300px;
-  padding: 100px;
-}
 
 </style>
