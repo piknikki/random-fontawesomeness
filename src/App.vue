4ad66289d4110b750fd7d885963a4028a966bf9e
Nikki Petersen <piknik@me.com>
2020-08-12 09:53:24 -0600

    basic outline

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="logo" alt="Vue logo" src="./assets/logo.png">
+  <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import Home from '@/components/Home'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Home }
 }
 </script>
 
@@ -24,5 +22,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#logo {
+  width: 100px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Random Fontawesome Icon Generator</h1>
+    <button class="button is-normal">Make it so</button>
+    <br>
+      <span class="icon is-large">
+        <i class="fal fa-magic is-large"></i>
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      icon: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
